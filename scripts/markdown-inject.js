51e1302afb6fab565ed93c18aad7f152ed006c45
Seth Carbon <github.com@genkisugi.net>
2017-08-06 20:16:51 -0700

    placeholder for injector

---
+++ scripts/markdown-inject.js
@@ -0,0 +1,115 @@
+////
+//// Temporarily create a static page from a GH-flavored Markdown
+//// file; still want the one-page app.
+////
+//// Example usage:
+////  node ./scripts/markdown-inject.js -i ./docs/criteria.md -o ./docs/criteria.html
+////
+
+var us = require('underscore');
+var fs = require("fs");
+var yaml = require('yamljs');
+var mustache = require('mustache');
+
+///
+/// Helpers.
+///
+
+// Aliases.
+var each = us.each;
+
+function _ll(arg1){
+    console.log(arg1); 
+}
+
+var debug = false;
+function _debug(arg1){
+    if( debug ){
+	console.log(arg1);
+    }
+}
+
+// Two or one  args.
+function _die(m1, m2){
+    if( typeof(m2) !== 'undefined' ){
+	console.error('ERROR:', m1, m2);
+    }else{
+	console.error('ERROR:', m1);
+    }
+    process.exit(-1);
+}
+
+///
+/// Opts.
+///
+
+var argv = require('minimist')(process.argv.slice(2));
+
+// Add extra "debugging" messages.
+debug = argv['d'] || argv['debug'];
+if( debug ){
+    _debug('Debugging is on.');
+}else{
+}
+
+//
+var in_data = argv['i'] || argv['in'];
+if( ! in_data ){
+    _die('Option (i|in) is required.');
+}else{
+    _debug('Will use input JSON file: ' + in_data);
+}
+
+//
+var in_tmpl = argv['t'] || argv['template'];
+if( ! in_tmpl ){
+    _die('Option (t|template) is required.');
+}else{
+    _debug('Will use input JSON file: ' + in_tmpl);
+}
+
+//
+var out_file = argv['o'] || argv['out'];
+if( ! out_file ){
+    _die('Option (o|out) is required.');
+}else{
+    _debug('Will use output file: ' + out_file);
+}
+
+///
+/// Main.
+///
+
+var data_sources = JSON.parse(fs.readFileSync(in_data, 'utf-8'));
+_debug('data', data_sources);
+
+// Goose the data so we have a single html-displayable string for the
+// license commentary.
+us.each(data_sources, function(source){
+    var cache = [];
+    us.each(source['license-commentary'], function(comment){
+	cache.push(comment);
+    });
+    source['license-commentary-embeddable'] = cache.join('\n\\n<hr />');
+});
+
+// Pug/Jade for table.
+var html_table_str = pug.renderFile('./scripts/static-table.pug',
+				    {"data_sources": data_sources});
+console.log('===');
+console.log(html_table_str);
+console.log('===');
+
+// Mustache for final.
+var template = fs.readFileSync(in_tmpl, 'utf-8');
+_debug('template', template);
+
+var outstr = mustache.render(template, {
+    "jsondata": JSON.stringify(data_sources),
+    "htmltablestr": html_table_str,
+    "tabledata": data_sources
+});
+
+fs.writeFileSync(out_file, outstr);
+
+
